c860963584f9bc28f35ae20bc9124c28b0a30ed0
unknown <thanhitutc@gmail.com>
2018-01-19 17:25:53 +0700

    init project

---
+++ app/src/main/java/com/framgia/music5/screen/MainActivity.java
@@ -0,0 +1,15 @@
+package com.framgia.music5.screen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.framgia.music5.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
